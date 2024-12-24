1e3e56b6bd78ed6b8dd7924fe63384e58ff36af1
Nicklaus <nicklaus.fangmeier@gmail.com>
2024-12-23 18:46:13 -0600

    fix: argument order

--- scripts/analyze.sh
+++ scripts/analyze.sh
@@ -9,4 +9,4 @@ fi
 rojo sourcemap default.project.json -o sourcemap.json
 curl -O https://raw.githubusercontent.com/JohnnyMorganz/luau-lsp/main/scripts/globalTypes.d.luau
 
-luau-lsp analyze src/ --sourcemap=sourcemap.json --defs=globalTypes.d.lua --ignore=Packages/
+luau-lsp analyze --sourcemap=sourcemap.json --defs=globalTypes.d.luau --ignore=Packages/ src/
